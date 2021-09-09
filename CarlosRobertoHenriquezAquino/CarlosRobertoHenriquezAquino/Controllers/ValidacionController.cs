36ab3421d2a9558640ed53bdf1c5237f5386dc70
ha0372 <69734226+ha0372@users.noreply.github.com>
2021-09-09 12:21:22 -0600

    ul

---
+++ CarlosRobertoHenriquezAquino/CarlosRobertoHenriquezAquino/Controllers/ValidacionController.cs
@@ -0,0 +1,41 @@
+﻿using CarlosRobertoHenriquezAquino.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarlosRobertoHenriquezAquino.Controllers
+{
+    public class ValidacionController : Controller
+    {
+        // GET: Validacion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Mensaje(ValidacionViewModel val)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Retiro", val);
+            }
+
+            return Redirect("Index");
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        public ActionResult Retiro()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ CarlosRobertoHenriquezAquino/CarlosRobertoHenriquezAquino/Models/ViewModel/ValidacionViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarlosRobertoHenriquezAquino.Models.ViewModel
+{
+    public class ValidacionViewModel
+    {
+
+        [Display(Name = "Monto")]
+        [Required(ErrorMessage = "Campo obligatorio")]
+        public int Monto { get; set; }
+
+    }
+}
